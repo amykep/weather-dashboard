08bee4541c7695fff520fbc3af448eeb72e9980b
AK <keppleramy@gmail.com>
2021-09-11 17:05:54 -0700

    added display function and search function to search history

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # weather-dashboard
+This application provides travelers with weather outlook for multiple citie to plan their trip accordingly
+
+
+### Key Elements
+In this app I deployed the following:
+1. Bootstrap
+2. JavaScript 
+3. Fetch API
+4. jQuery ajax function
+5. LocalStorage
+6. CSS
+
+### Development Outline
+1. Builded a search function for users to search the city of their choice, and save the search on localstorage.
+2. Created a function to check if the city has been searched before by the user. If searched before, it will not be added again to the search history, and its data will be displayed.
+3. Fetched the longitude and latitude of the searched city, then deployed another fetch (all-in-one) using the longitude and latitude coordinates to get the UV Index. I noticed that the UVIndex info became only available for Open Weather subscibers., so it always display zero even when I used their provided example I was still getting a zero.
+4. Worked on building the structure for displaying the current and Forecast weather.
+5. Added addEventListener to each city in the history so that it is searched again on click.
+
+
+### Link to the deployed application: https://amykep.github.io/weather-dashboard/
+
+### Screenshot of the Daily Calendar:
+<img src=".assets/images/weather-dashboard.png">
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,37 +1,63 @@
 const apiKey = "25437cbe4ce85e57e46977492075f305";
+const weekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+city = {}
 
-function main()
+function getDateInfo()
 {
-
+    var todayDate = new Date();
+    var tempdate = {
+        "currentYear": todayDate.getFullYear(),
+        "currentMonth": todayDate.getMonth(),
+        "currentDay": todayDate.getDay(),
+        "currentDate": todayDate.getDate(),
+        "currentHour": todayDate.getHours()
+    }
+    return tempdate
 }
 
 function saveSearch(eventData)
 {
     eventData.preventDefault()
+
     console.log(eventData)
     console.log("Target:", this)
 
 
     // get data from input box
-    var searchInput = $("#searchInput").val();
+    var searchInput = $("#searchInput").val().trim();
     console.log("search", searchInput);
 
     if (searchInput !== "" && searchInput !== null)
     {
-        // if there is nothing saved at the start then save an empty array
-        if (localStorage.getItem("cities") == null)
+        var localInfo = localStorage.getItem("cities")
+
+        if (localInfo !== "" && localInfo !== null && (JSON.parse(localInfo)).includes(searchInput))
         {
-            localStorage.setItem("cities", "[]");
+            // console.log("Skip")
+            displaySearch();
+            document.getElementById('searchInput').value = null;
+            getCitiWeather(searchInput)
+            return
         }
+        else
+        {
+            // if there is nothing saved at the start then save an empty array
+            if (localStorage.getItem("cities") == null)
+            {
+                localStorage.setItem("cities", "[]");
+            }
 
-        // get old data and add the new data to it
-        var oldSearchHistory = JSON.parse(localStorage.getItem("cities"));
-        oldSearchHistory.push(searchInput);
+            // get old data and add the new data to it
+            var oldSearchHistory = JSON.parse(localStorage.getItem("cities"));
+            oldSearchHistory.push(searchInput);
 
-        // save the old + new data to local storage
-        localStorage.setItem("cities", JSON.stringify(oldSearchHistory));
+            // save the old + new data to local storage
+            localStorage.setItem("cities", JSON.stringify(oldSearchHistory));
 
-        getCitiWeather(searchInput)
+
+            getCitiWeather(searchInput)
+        }
     }
     else
     {
@@ -41,23 +67,30 @@ function saveSearch(eventData)
     displaySearch();
 };
 
+function refreshInput(eventData)
+{
+    // console.log(this)
+    document.getElementById('searchInput').value = this.textContent
+}
 
-function displaySearch()
+function displaySearch(displayDate)
 {
     if (localStorage.getItem("cities") !== null)
     {
         //  document.createElement("ul")
         var historyList = document.querySelector("#searchHistory");
-
+        historyList.innerHTML = '';
 
         var oldSearchHistory = JSON.parse(localStorage.getItem("cities"));
         console.log("history", oldSearchHistory);
 
-        for (let i = 0; i <= oldSearchHistory.length; i++)
+
+        for (let i = 0; i < oldSearchHistory.length; i++)
         {
             var historyItem = document.createElement("li");
             historyItem.textContent = oldSearchHistory[i];;
             historyItem.className = "searchItem";
+            historyItem.addEventListener("click", refreshInput)
             historyList.appendChild(historyItem);
         }
 
@@ -66,12 +99,12 @@ function displaySearch()
 
 function getCitiWeather(city)
 {
-    city = city.trim()
     city = city.replace(/\s/g, "%20");
 
     console.log("city", city)
 
     var apiurl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&Appid=" + apiKey;
+
     console.log(apiurl)
 
     try
@@ -83,7 +116,8 @@ function getCitiWeather(city)
                 response.json().then(function (data)
                 {
                     console.log(data)
-                    // displayCitiWeather(city, data)
+                    getCitiUVIndex(city, data)
+                    displayForecast(city)
 
                 });
             }
@@ -98,16 +132,131 @@ function getCitiWeather(city)
     {
         alert("Unable to connect to OpenWeather");
     }
+
 };
-function displayCitiWeather(city, data)
+
+function getCitiUVIndex(city, data)
 {
-    console.log("data2", data)
-    console.log("lon", data.coord.lon)
-    console.log("lat", data.coord.lat)
-    console.log("temp", data.main.temp)
-    console.log("wind", data.wind.speed)
-    console.log("hum", data.main.humidity)
-    // var apiURLOneCall = 
+    var apiurl2 = "https://api.openweathermap.org/data/2.5/onecall?lat=" + data.coord.lat + "&lon=" + data.coord.lon + "&appid=" + apiKey + "&units=imperial"
+
+    console.log(apiurl2)
+
+    try
+    {
+        fetch(apiurl2).then(function (response)
+        {
+            if (response.ok)
+            {
+                response.json().then(function (data3)
+                {
+                    console.log("data3", data3)
+                    displayCurrentWeather(city, data3)
+                });
+            }
+            else
+            {
+                alert("Error, please check your entry")
+            }
+
+        })
+    }
+    catch (err)
+    {
+        alert("Unable to connect to OpenWeather");
+    }
+};
+
+function getDetails(data)
+{
+    var temp = {
+        "Temperature": JSON.stringify(data.current.temp),
+        "Humidity": JSON.stringify(data.current.humidity) + "%",
+        "WindSpeed": JSON.stringify(data.current.wind_speed),
+        "UV Index": JSON.stringify(data.current.uvi),
+        "WeatherIcon": data.current.weather[0].icon,
+    }
+
+    var weatherIcon = data.current.weather[0].icon;
+    console.log("icon", weatherIcon)
+    console.log("temp", temp.WeatherIcon)
+
+    return temp;
+}
+
+function addItem(key, ARR, weatherList)
+{
+    var listItem = document.createElement("li");
+    listItem.className = "currentList";
+    listItem.textContent = key + ": " + ARR[key];
+    weatherList.appendChild(listItem);
+}
+
+function displayCurrentWeather(city, data)
+{
+    city = city.replace("%20", " ");
+
+    var dateInfo = getDateInfo()
+    var currentWeatherDataObj = getDetails(data)
+
+    var weatherList = document.querySelector(".cityCurrentData");
+
+    weatherList.innerHTML = "";
+
+    var fullDate = "(" + weekDays[dateInfo.currentDay] + ", " + months[dateInfo.currentMonth] + " " + dateInfo.currentDate + ", " + dateInfo.currentYear + ")";
+
+    weatherList.innerHTML = "<span style='font-weight:bolder; font-size: x-large;' >" + city + "   " + fullDate + "</span>"
+
+
+    for (const key in currentWeatherDataObj)
+    {
+        if (key != "WeatherIcon")
+        {
+            addItem(key, currentWeatherDataObj, weatherList)
+        }
+    }
+
+    var weatherIcon = data.current.weather[0].icon;
+    var imgSource = "https://openweathermap.org/img/wn/" + weatherIcon + "@2x.png"
+
+    var weatherIconImg = document.createElement("img")
+    weatherIconImg.setAttribute("src", imgSource)
+
+    console.log(" weatherList.childNodes", weatherList.childNodes)
+    weatherList.insertBefore(weatherIconImg, weatherList.childNodes[1]);
+    console.log(" weatherList.childNodes 2", weatherList.childNodes)
+
+}
+
+// 5-day forecast 
+function displayForecast(city)
+{
+    $.ajax({
+
+        url: "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&Appid=" + apiKey + "&units=imperial",
+
+        // method: "GET"
+    }).then(function (response)
+    {
+        console.log("response", response)
+        // Array for 5-days - Call was for 5 day / 3 hour forecast data
+        var day = [0, 8, 16, 24, 32];
+        var fiveDayDiv = $(".fiveDayOne")
+        fiveDayDiv.empty();
+        // For each for 5 days
+        day.forEach(function (i)
+        {
+            // in response JSON, you've a list and inside each object they are passing the timestamp. To convert that time to human readable, you can multiple by 1000(milliseconds) and use the DateTime constructor to convert.
+
+
+            var FiveDayTimeUTC1 = new Date(response.list[i].dt * 1000);
+            FiveDayTimeUTC1 = FiveDayTimeUTC1.toLocaleDateString("en-US");
+
+            fiveDayDiv.append("<div class=fiveDayColor>" + "<p>" + FiveDayTimeUTC1 + "</p>" + `<img src="https://openweathermap.org/img/wn/${response.list[i].weather[0].icon}@2x.png">` + "<p>" + "Temperature: " + response.list[i].main.temp + "</p>" + "Wind Speed: " + response.list[i].wind.speed + "</p>" + "<p>" + "Humidity: " + response.list[i].main.humidity + "%" + "</p>" + "</div>");
+
+
+        })
+
+    });
 }
 
 $(".search").click(saveSearch)
